0c064815ed350ea4a9b8730e86dff35671c3738f
phenix1229 <paul.garay@codeimmersives.com>
2020-02-20 16:00:34 -0500

    app.js complete?

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const port = process.env.PORT || '3000';
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/', userRoutes);
+
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 });
